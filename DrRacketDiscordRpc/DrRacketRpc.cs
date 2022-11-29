d777233bf161e03ac527e6f7a7add6ef1435f22c
kna27 <krisharora27@gmail.com>
2022-11-29 13:38:13 -0500

    Check if process is running

---
+++ DrRacketDiscordRpc/DrRacketRpc.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+public class DrRacketRpc
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            if (Process.GetProcessesByName("DrRacket").Length > 0)
+            {
+                Process[] racketProcesses = Process.GetProcessesByName("DrRacket");
+            }
+            Task.Delay(10000).GetAwaiter().GetResult();
+        }
+    }
+}
+
