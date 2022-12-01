2d1a3fa8fd523661493f4219f6fd0fa18a9a0a31
kna27 <krisharora27@gmail.com>
2022-11-30 21:09:34 -0500

    Create DiscordRPC

--- DrRacketDiscordRpc/DrRacketRpc.cs
+++ DrRacketDiscordRpc/DrRacketRpc.cs
@@ -1,15 +1,38 @@
-﻿using System.Diagnostics;
+﻿using DiscordRPC;
+using System.Diagnostics;
 
 public class DrRacketRpc
 {
+    static DiscordRpcClient? client;
+    static RichPresence? rp;
     public static void Main()
+
     {
+        client = new DiscordRpcClient("");
+        client.Initialize();
+        rp = new RichPresence
+        {
+            Assets = new Assets()
+            {
+                LargeImageKey = "racket",
+                LargeImageText = "DrRacket",
+            }
+        };
+        rp.Details = "Editing File";
+        client.SetPresence(rp);
         while (true)
         {
             if (Process.GetProcessesByName("DrRacket").Length > 0)
             {
-                Process[] racketProcesses = Process.GetProcessesByName("DrRacket");
+                Process racketProcess = Process.GetProcessesByName("DrRacket")[0];
+                client.SetPresence(rp);
+                Console.WriteLine(racketProcess.MainWindowTitle);
             }
+            else
+            {
+
+            }
+
             Task.Delay(10000).GetAwaiter().GetResult();
         }
     }
